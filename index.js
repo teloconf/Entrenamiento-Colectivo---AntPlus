4767b6df17c6c2c7387d19d63052b54cc9c43c11
Gabriel Urresta <gabrielurresta@MacBook-Pro-de-Gabriel.local>
2017-05-31 12:22:59 +0200

    AntPlus Init

---
+++ index.js
@@ -0,0 +1,82 @@
+var Ant = require('ant-plus');
+var stick = new Ant.GarminStick2;
+var sensor1 = new Ant.HeartRateSensor(stick);
+var dev_id = 0;
+sensor1.on('hbdata', function (data) {
+  if(data.DeviceID == '43736'){
+    var nombre = 'Rafa';
+  }
+  if(data.DeviceID == '43734'){
+    var nombre = 'Gabri';
+  }
+  if(data.DeviceID == '43731'){
+    var nombre = 'Nacho';
+  }
+	console.log(nombre + ': ', data.DeviceID, data.ComputedHeartRate);
+	if (data.DeviceID !== 0 && dev_id === 0) {
+		dev_id = data.DeviceID;
+		console.log('detaching...');
+		sensor1.detach();
+		sensor1.once('detached', function () {
+			sensor1.attach(0, dev_id);
+		});
+	}
+});
+
+sensor1.on('attached', function () { console.log('sensor1 attached'); });
+sensor1.on('detached', function () { console.log('sensor1 detached'); });
+
+var sensor2 = new Ant.StrideSpeedDistanceSensor(stick);
+
+sensor2.on('ssddata', function (data) {
+	console.log('sensor 2: ', data.DeviceID, data);
+});
+
+sensor2.on('attached', function () { console.log('sensor2 attached'); });
+sensor2.on('detached', function () { console.log('sensor2 detached'); });
+
+var scanner = new Ant.HeartRateScanner(stick);
+
+scanner.on('hbdata', function (data) {
+  if(data.DeviceID == '43736'){
+    var nombre = 'Rafa';
+  }
+  if(data.DeviceID == '43734'){
+    var nombre = 'Gabri';
+  }
+  if(data.DeviceID == '43731'){
+    var nombre = 'Nacho';
+  }
+	console.log(nombre + ' ', data.DeviceID, data.ComputedHeartRate);
+});
+
+scanner.on('attached', function () { console.log('scanner attached'); });
+scanner.on('detached', function () { console.log('scanner detached'); });
+
+stick.on('startup', function () {
+	console.log('startup');
+
+	console.log('Max channels:', stick.maxChannels);
+
+	sensor1.attach(0, 0);
+
+	setTimeout(function (data) {
+		sensor2.attach(1, 0);
+	}, 2000);
+
+	setTimeout(function () {
+		sensor1.once('detached', function () { sensor2.detach(); });
+		sensor2.once('detached', function () {
+			scanner.scan();
+		});
+		sensor1.detach();
+	}, 30000);
+});
+
+stick.on('shutdown', function () { console.log('shutdown'); });
+
+if (!stick.open()) {
+	console.log('Stick not found!');
+} else {
+
+}
